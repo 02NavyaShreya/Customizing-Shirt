6d39a45ef2c8fd6ede1b36cab60c54ea1ce1b923
02NavyaShreya <121332541+02NavyaShreya@users.noreply.github.com>
2025-01-16 10:29:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust this path based on your project structure
+    "./public/index.html", // If applicable, add your public HTML file paths
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
